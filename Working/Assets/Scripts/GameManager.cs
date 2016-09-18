f612cdc8862eaaa1dba0806272ec1c053568cbef
CASPERNIRVANA\AliEnver <tiyago1@gmail.com>
2016-09-18 17:48:37 +0300

    Unity project intsaller

---
+++ Working/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	#region Fields
+
+    public Player[] Players;
+
+    public int[] Stones = new int[13] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    
+	#endregion //Fields
+	
+	#region Unity Methods
+	
+	private void Start () 
+	{
+		Initalize();
+	}
+	
+	private void Update () 
+	{
+	
+	}
+	
+	#endregion //Unity Methods
+	
+	#region Public Methods
+	
+	public void Initalize()
+	{
+        Player player = new Player();
+        player.Id = 0;
+        player.name = "First";
+	}
+	
+	#endregion // Public Methods
+	
+	#region Private Methods
+	
+	private void SamplePrivateMethods()
+	{
+	
+	}	
+	
+	#endregion //Private Methods
+}
+
+
+/*
+
+    Toplam 106 adet taş bulunmaktadır. 
+ *  1  - 13 taş numaraları vardır.
+ *  4 renk bulunmaktadır. 
+ *  Her sayıdan iki tane var
+ *  2 tanede siyah " Sahte Okey " vardır.
+ *  
+
+*/
---
+++ Working/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    public int Id;
+    public string Name;
+    public bool IsPlaying;
+    public int[] Stones;
+}
